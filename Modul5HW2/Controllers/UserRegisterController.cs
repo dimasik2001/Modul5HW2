930ab51fb1334f7259af43c522365bb2260da2bc
Dimasik2001 <dimonkalibaba@gmail.com>
2021-09-24 16:57:27 +0300

    HW

---
+++ Modul5HW2/Controllers/UserRegisterController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Modul5HW2.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class UserRegisterController : ControllerBase
+    {
+        [HttpPost]
+        [Route("[Action]")]
+        public string Regist([FromForm]User user)
+        {
+            return $"{user}, Hello";
+        }
+    }
+    public class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName}";
+        }
+    }
+}
